37c601e82e1266465d6f8209acf569e85d29f9df
Noah <noahschnur@gmail.com>
2021-04-05 14:57:49 -0700

    initial

---
+++ index.js
@@ -0,0 +1,8 @@
+const style = require("./dist/style.css");
+const staff = require("./lib/Employee.js");
+const manager = require("./lib/Manager.js");
+const intern = requrie("./lib/Intern.js");
+const engineer = require("lib/Engineer.js");
+const run = require("./src.htmlTemplate.js");
+
+new run().initializeRun();
